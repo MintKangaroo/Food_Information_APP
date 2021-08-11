f56437f90fa23e5b9c76a173a267800d3fdcd0bf
MintKangaroo <yjun273@gmail.com>
2021-08-11 23:06:01 +0900

    TODO, FIXME 하이라이트테스트

--- food_information_app_project/lib/main.dart
+++ food_information_app_project/lib/main.dart
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food Information', //TODO "앱이름 정해서 변경하기"
-      theme: ThemeData(
-        primaryColor: Colors.white,
+      title: 'Food Information', //TODO: 앱이름 정해서 변경하기
+      theme: ThemeData( 
+        primaryColor: Colors.white, //FIXME: 앱테마 컬러 고치ㅣ
       ),
       home: MainPage(),
     );

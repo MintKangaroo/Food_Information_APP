4ce7415601e8efda30a7a1649c9ffe310022c68e
MintKangaroo <yjun273@gmail.com>
2021-08-19 23:22:41 +0900

    폰트 변경

--- food_information_app_project/lib/MyPage.dart
+++ food_information_app_project/lib/MyPage.dart
@@ -9,35 +9,75 @@ class MyPage extends StatelessWidget {
     var displayHeight = MediaQuery.of(context).size.height;
     return SafeArea(
         child: Scaffold(
+          appBar: (
+            
+            AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+            )
+          ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.person,
-                    size: 250,
-                  ),
-                ],
+        child: Container(
+          color : Colors.white,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      size: 250,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              "ID : ",
-              style: TextStyle(fontSize: 25),
-            ),
-            Text(
-              "소개글",
-              style: TextStyle(fontSize: 25),
-            ),
-            Text(
-              "내가 다녀간 곳 리스트",
-              style: TextStyle(fontSize: 25),
-            ),
-            Container(
-              decoration: BoxDecoration(
+              Text(
+                "ID : ",
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                "소개글",
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                "내가 다녀간 곳 리스트",
+                style: TextStyle(fontSize: 25),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(3.0, 3.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 1.0,
+                      ),
+                    ],
+                    color: Colors.red[200],
+                    borderRadius: BorderRadius.circular(30),),
+                width: displayWidth * 0.95,
+                height: displayHeight * 0.1,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "맛집 1",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30,),
+              Container(
+                decoration: BoxDecoration(
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey,
@@ -47,95 +87,65 @@ class MyPage extends StatelessWidget {
                     ),
                   ],
                   color: Colors.red[200],
-                  borderRadius: BorderRadius.circular(30),),
-              width: displayWidth * 0.95,
-              height: displayHeight * 0.1,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "맛집 1",
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                width: displayWidth * 0.95,
+                height: displayHeight * 0.1,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "맛집 1",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 30,),
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(3.0, 3.0),
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
-                color: Colors.red[200],
-                borderRadius: BorderRadius.circular(30),
+              SizedBox(
+                height: 30,
               ),
-              width: displayWidth * 0.95,
-              height: displayHeight * 0.1,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "맛집 1",
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ],
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(3.0, 3.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 1.0,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(3.0, 3.0),
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
-                color: Colors.red[200],
-                borderRadius: BorderRadius.circular(30),
-              ),
-              width: displayWidth * 0.95,
-              height: displayHeight * 0.1,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "맛집 1",
-                          style: TextStyle(fontSize: 25),
-                        ),
-                      ],
+                  ],
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                width: displayWidth * 0.95,
+                height: displayHeight * 0.1,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "맛집 1",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
         ),
       ),
     ));
